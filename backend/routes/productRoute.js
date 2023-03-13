445fc6aa69cac0ba3db2acd3970322a1add8c239
Milos1988 <vojvoda1988@gmail.com>
2023-03-14 00:13:53 +0100

    finish complet pagination

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,7 +6,6 @@ productRoute.post("/get-product", async (req, res) => {
   const countQuery = await ProductModel.where({}).countDocuments();
   let pagination = req.body;
   console.log(pagination)
-  console.log(pagination)
   ProductModel.find({})
     .skip(pagination.start * pagination.perPage)
     .limit(pagination.perPage)
--- frontend/src/components/productListComponent.jsx/ProductListComponent.jsx
+++ frontend/src/components/productListComponent.jsx/ProductListComponent.jsx
@@ -12,6 +12,7 @@ const ProductListComponent = () => {
   });
   const [arrayPagination, setArrayPagination] = useState([]);
   const selectArray = [4, 8, 16, 24, 48];
+  const [active, setActive ] = useState(1)
 
   useEffect(() => {
     getProductData(pagination)
@@ -20,18 +21,20 @@ const ProductListComponent = () => {
         setProduct(res.data.data);
         const paginationAllNumber = Math.ceil(
           numberPagination / pagination.perPage
-        );
-        setPagination({ ...pagination, allPag: paginationAllNumber })
+          );
+          // console.log(paginationAllNumber )
+        let copyPagination = { ...pagination, allPag: paginationAllNumber };
+          setPagination(copyPagination)
         setArrayPagination(
           Array.from({ length: paginationAllNumber }, (v, k) => k + 1)
         );
       })
       .catch((err) => console.log(err));
-  }, [pagination.perPage, pagination.start]);
+  }, [pagination.perPage, pagination.start, pagination.allPag]);
 
   return (
     <>
-      <div className="row container d-flex gap-4 flex-wrap">
+      <div className="row container d-flex m-auto flex-wrap">
         {product?.map((el, index) => {
           return (
             <ProductComponent
@@ -43,7 +46,7 @@ const ProductListComponent = () => {
           );
         })}
       </div>
-      <div className="row ">
+      <div className="row mt-5">
         <div className="col-2 offset-2">
           <div className="dropdown">
             <button
@@ -78,23 +81,24 @@ const ProductListComponent = () => {
           <ul className="pagination justify-content-start">
             <li className="page-item">
               <Link className="page-link" to="#" aria-label="Previous" onClick={() => {
-                if (pagination.start === 0) {
-                  setPagination({ ...pagination, start: 0 });
-                } else {
+                if (pagination.start > 0) {
                   setPagination({ ...pagination, start: pagination.start - 1 });
+                  setActive(active - 1);
                 }
               }}>
                 <span aria-hidden="true">&laquo;</span>
               </Link>
             </li>
             {arrayPagination?.map((el, index) => {
+              
               return (
-                <li key={index} className="page-item">
-                  <Link className="page-link" onClick={() => {
-                    {
-                      setPagination({ ...pagination, start: el - 1 });
-                    }
-                  }}>
+                <li key={index} className={`page-item ${el === active ? "active" : ""}`} onClick={(e) => {
+                  {
+                    setPagination({ ...pagination, start: el - 1 });
+                    setActive(Number( e.target.text));
+                  }
+                }}>
+                  <Link className="page-link" >
                     {el}
                   </Link>
                 </li>
@@ -103,13 +107,9 @@ const ProductListComponent = () => {
 
             <li className="page-item">
               <Link className="page-link" href="#" aria-label="Next" onClick={() => {
-                console.log(pagination.start)
-                console.log(pagination.allPag)
-                console.log(pagination.start >= pagination.allPag)
-                if (pagination.start + 1 > pagination.allPag) {
-                  setPagination({ ...pagination, start: pagination.allPag });
-                } else {
-                  setPagination({ ...pagination, start: pagination.start + 1 });
+                if (pagination.start + 1< pagination.allPag) {
+                setPagination({ ...pagination, start: pagination.start + 1 });
+                setActive(active + 1);
                 }
 
               }}>
