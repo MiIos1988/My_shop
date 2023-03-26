fa25caabeee6ec1663f062bfff8715f4916cf1ad
Milos1988 <vojvoda1988@gmail.com>
2023-03-27 00:55:06 +0200

    Show product component - finished style

--- frontend/src/components/showProductComponent/ShowProductComponent.jsx
+++ frontend/src/components/showProductComponent/ShowProductComponent.jsx
@@ -1,37 +1,85 @@
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { getOneProductData } from "../../service/productService";
-import { MdKeyboardArrowDown } from 'react-icons/md';
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdKeyboardArrowUp } from "react-icons/md";
+import { SlBasket } from "react-icons/sl";
+import { BsSuitHeartFill } from "react-icons/bs";
+import { TbArrowsCross } from "react-icons/tb";
 
 const ShowProductComponent = () => {
-    const [queryParams] = useSearchParams();
-    const [product, setProduct]= useState()
+  const [queryParams] = useSearchParams();
+  const [product, setProduct] = useState();
+  const [quantity, setQuantity] = useState(1);
 
-useEffect(() => {
-  const id = queryParams.get("id");
-  getOneProductData(id).then( res => setProduct(res.data.data))
-  .catch(err => console.log(err))
-},[]
-)
+  useEffect(() => {
+    const id = queryParams.get("id");
+    getOneProductData(id)
+      .then((res) => setProduct(res.data.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-        <div className="container d-flex my-5 row mx-auto showProduct">
-            <div className="imageProduct border col-5">
-                <img src={product?.imgUrl} alt="" />
+  const increaseQuantity = () => {
+    setQuantity((prev) => prev + 1);
+    console.log(quantity);
+  };
+
+  const reduceQuantity = () => {
+    quantity > 1 ? setQuantity((prev) => prev - 1) : setQuantity(1);
+  };
+
+  return (
+    <div className="container d-flex my-5 row mx-auto showProduct">
+      <div className="imageProduct border col-5">
+        <img src={product?.imgUrl} alt="" />
+      </div>
+      <div className="infoProduct col-5 offset-1 ">
+        <h2>{product?.title}</h2>
+        <p className="price">${product?.price}</p>
+        <p className="description">{product?.description}</p>
+        <div className="quantity d-flex mb-4">
+          <p className="m-0 lh-lg">Quantity </p>
+          <div className="counter d-flex ps-4">
+            <div className="border p-2 reduce" onClick={reduceQuantity}>
+              <MdKeyboardArrowDown />
             </div>
-            <div className="infoProduct col-5 offset-1 ">
-                <h2>{product?.title}</h2>
-                <p className="price">${product?.price}</p>
-                <p className="description">{product?.description}</p>
-                <div className="quantity">
-                    <p>Quantity </p>
-                    <div><MdKeyboardArrowDown/></div>
-                    <div></div>
-                    <div></div>
-                </div>
+            <div className="border py-2 px-4 ">{quantity}</div>
+            <div className="border p-2 increase" onClick={increaseQuantity}>
+              <MdKeyboardArrowUp />
             </div>
+          </div>
         </div>
-    )
-}
+        <div className="productAction d-flex fs-5 mb-3 pb-3">
+          <div className="border p-2">
+            <SlBasket /> Add to cart
+          </div>
+          <div className="border p-2 mx-2">
+            <BsSuitHeartFill />
+          </div>
+          <div className="border p-2">
+            <TbArrowsCross />
+          </div>
+        </div>
+        <div className="socialNetwork d-flex">
+          <Link
+            to={"https://www.facebook.com/"}
+            target="_blank"
+            className="fb"
+          ></Link>
+          <Link
+            to={"https://twitter.com/"}
+            target="_blank"
+            className="tw"
+          ></Link>
+          <Link
+            to={"https://www.instagram.com/"}
+            target="_blank"
+            className="in"
+          ></Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ShowProductComponent;
