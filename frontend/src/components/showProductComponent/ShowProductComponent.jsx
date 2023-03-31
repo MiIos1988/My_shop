40017391dd9da964c09e67753973f1cbf98b735d
Milos1988 <vojvoda1988@gmail.com>
2023-03-31 13:51:40 +0200

    Product successfully

--- frontend/src/components/showProductComponent/ShowProductComponent.jsx
+++ frontend/src/components/showProductComponent/ShowProductComponent.jsx
@@ -8,6 +8,7 @@ import { TbArrowsCross } from "react-icons/tb";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../redux/cartSlicer";
 import QuantityProductComponent from "./component/QuantityProductComponent";
+import ModalProductComponent from "./component/ModalProductComponent";
 
 const ShowProductComponent = () => {
   const [queryParams] = useSearchParams();
@@ -29,7 +30,7 @@ const ShowProductComponent = () => {
       .catch((err) => console.log(err));
   }, [idParams]);
 
-  
+
 
   const mouseMoveIn = (e) => {
     setZoomOut(true);
@@ -43,66 +44,74 @@ const ShowProductComponent = () => {
 
   const addProductInCart = () => {
     dispatch(addToCart({ id: product._id, imgUrl: product.imgUrl, title: product.title, price: product.price, quantity: quantity }));
+    openModal()
 
   }
 
+  const openModal = () => {
+    console.log("work")
+  }
+
   return (
-    <div className="container d-flex my-5 row mx-auto showProduct">
-      <div
-        className="imageProduct border col-5"
-        onMouseMove={mouseMoveIn}
-        onMouseLeave={mouseMoveExit}
-      >
-        <img
-          src={product?.imgUrl}
-          alt=""
-          style={
-            zoomOut
-              ? {
-                transformOrigin: `${moveX}px ${moveY}px`,
-                transform: "scale(2)",
-              }
-              : { transformOrigin: "center", transform: "scale(1)" }
-          }
-        />
-      </div>
-      <div className="infoProduct col-5 offset-1 ">
-        <h2>{product?.title}</h2>
-        <p className="price">${product?.price}</p>
-        <p className="description">{product?.description}</p>
+    <>
+      <div className="container d-flex my-5 row mx-auto showProduct">
+        <div
+          className="imageProduct border col-5"
+          onMouseMove={mouseMoveIn}
+          onMouseLeave={mouseMoveExit}
+        >
+          <img
+            src={product?.imgUrl}
+            alt=""
+            style={
+              zoomOut
+                ? {
+                  transformOrigin: `${moveX}px ${moveY}px`,
+                  transform: "scale(2)",
+                }
+                : { transformOrigin: "center", transform: "scale(1)" }
+            }
+          />
+        </div>
+        <div className="infoProduct col-5 offset-1 ">
+          <h2>{product?.title}</h2>
+          <p className="price">${product?.price}</p>
+          <p className="description">{product?.description}</p>
 
-        <QuantityProductComponent  quantity={quantity} setQuantity={setQuantity} />
+          <QuantityProductComponent quantity={quantity} setQuantity={setQuantity} />
 
-        <div className="productAction d-flex fs-5 mb-3 pb-3">
-          <div className="border p-2" onClick={addProductInCart}>
-            <SlBasket /> Add to cart
+          <div className="productAction d-flex fs-5 mb-3 pb-3">
+            <div className="border p-2" onClick={addProductInCart}>
+              <SlBasket /> Add to cart
+            </div>
+            <div className="border p-2 mx-2">
+              <BsSuitHeartFill />
+            </div>
+            <div className="border p-2">
+              <TbArrowsCross />
+            </div>
           </div>
-          <div className="border p-2 mx-2">
-            <BsSuitHeartFill />
-          </div>
-          <div className="border p-2">
-            <TbArrowsCross />
+          <div className="socialNetwork d-flex">
+            <Link
+              to={"https://www.facebook.com/"}
+              target="_blank"
+              className="fb"
+            ></Link>
+            <Link
+              to={"https://twitter.com/"}
+              target="_blank"
+              className="tw"
+            ></Link>
+            <Link
+              to={"https://www.instagram.com/"}
+              target="_blank"
+              className="in"
+            ></Link>
           </div>
         </div>
-        <div className="socialNetwork d-flex">
-          <Link
-            to={"https://www.facebook.com/"}
-            target="_blank"
-            className="fb"
-          ></Link>
-          <Link
-            to={"https://twitter.com/"}
-            target="_blank"
-            className="tw"
-          ></Link>
-          <Link
-            to={"https://www.instagram.com/"}
-            target="_blank"
-            className="in"
-          ></Link>
-        </div>
       </div>
-    </div>
+      <ModalProductComponent product={product} quantity={quantity}/>
+    </>
   );
 };
 
---
+++ frontend/src/components/showProductComponent/component/ModalProductComponent.jsx
@@ -0,0 +1,42 @@
+import { BsCheckLg } from 'react-icons/bs'
+import { RiCloseFill } from 'react-icons/ri'
+import { Link } from 'react-router-dom'
+
+const ModalProductComponent = ({ product, quantity }) => {
+    console.log(product)
+    return (
+        <div className="modalDialog row d-flex align-items-center">
+            <div className="modalField border p-0">
+                <div className="header d-flex justify-content-center align-items-center">
+                    <div className='d-flex align-items-center'>
+                        <BsCheckLg />
+                        <p className='m-0 p-3'>Product successfully added to your shopping cart</p>
+                        <RiCloseFill className='ms-5' />
+                    </div>
+                </div>
+                <div className='product d-flex'>
+                    <div className='imageField'><img src={product?.imgUrl} alt="" /></div>
+                    <div className='border-end titleField'>
+                        <h4 className='mt-3'>{product?.title}</h4>
+                        <br />
+                        <p><b>Price:</b>  ${product?.price}</p>
+
+                        <p><b>Quantity:</b>  ${quantity}</p>
+                    </div>
+                    <div className='endField p-5' >
+                        <p><b>Total price: </b> {product?.price * quantity}</p>
+                        <p><b>Total shipping: </b> $0.00</p>
+                        <p><b>Taxes: </b> $0.00</p>
+                        <p><b>Total: </b>${product?.price * quantity}</p>
+                        <div>
+                            <Link className='btn btn-secondary me-5' to={"/shop"}>Continue Shopping </Link>
+                            <Link className='btn btn-dark' to={"/cart-shopp"}>Continue Shopping </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ModalProductComponent;
