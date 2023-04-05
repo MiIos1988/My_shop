8cc3bd43ca6f0bd86c80668cb5c95011e50761ad
Milos1988 <vojvoda1988@gmail.com>
2023-04-06 01:44:46 +0200

    Finished resposive in home page

--- frontend/src/components/sliderProductComponent/SliderProductComponent.jsx
+++ frontend/src/components/sliderProductComponent/SliderProductComponent.jsx
@@ -12,54 +12,73 @@ import { toggleLoader } from "../../redux/loaderSlicer";
 import { useDispatch } from "react-redux";
 
 const SliderProductComponent = () => {
-  const [product, setProduct] = useState([])
+  const [product, setProduct] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(toggleLoader(true))
-    getProductData({ start: 0, perPage: 16 }).then(res => {
-      setProduct(res.data.data)
-      dispatch(toggleLoader(false))
-    })
+    dispatch(toggleLoader(true));
+    getProductData({ start: 0, perPage: 16 })
+      .then((res) => {
+        setProduct(res.data.data);
+        dispatch(toggleLoader(false));
+      })
 
-      .catch(err => console.log(err))
-  }, []
-  )
-
-
+      .catch((err) => console.log(err));
+  }, []);
+  const arraySlider = Array.from({ length: 8 }, (el, i) => i + 1);
   return (
     <div className="container mx-auto sliderProduct">
       <Swiper
         loop={true}
-        slidesPerView={4}
+        slidesPerView={1}
         spaceBetween={30}
         freeMode={true}
         navigation={true}
         modules={[FreeMode, Navigation]}
         className="mySwiperProduct"
+        breakpoints={{
+          // when window width is >= 640px
+          500: {
+            slidesPerView: 2,
+          },
+          // when window width is >= 640px
+          768: {
+            slidesPerView: 3,
+          },
+          // when window width is >= 768px
+          992: {
+            slidesPerView: 4,
+          },
+        }}
       >
-        {/* {product[index]?.title} */}
-        {/* {product[index + 7]?.title} */}
-        {[1, 2, 3, 4, 5, 6, 7, 8].map((el, index) => {
-          return <SwiperSlide key={index}>
-            <div className="d-flex flex-column ">
-              <div className="pr1 mt-5" >
-                <ProductComponent imgUrl={product[index]?.imgUrl} title={product[index]?.title} price={product[index]?.price} id={product[index]?._id} />
+        {arraySlider.map((el, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className="d-flex flex-column ">
+                <div className="pr1 mt-5">
+                  <ProductComponent
+                    imgUrl={product[index]?.imgUrl}
+                    title={product[index]?.title}
+                    price={product[index]?.price}
+                    id={product[index]?._id}
+                  />
+                </div>
+                <div className="pr2 ">
+                  <ProductComponent
+                    imgUrl={product[index + 8]?.imgUrl}
+                    title={product[index + 7]?.title}
+                    price={product[index + 8]?.price}
+                    id={product[index + 8]?._id}
+                  />
+                </div>
+                {/* </div> */}
               </div>
-              <div className="pr2 ">
-                <ProductComponent imgUrl={product[index + 8]?.imgUrl} title={product[index + 7]?.title} price={product[index + 8]?.price} id={product[index + 8]?._id} />
-              </div>
-              {/* </div> */}
-            </div>
-          </SwiperSlide>
-        }
-        )}
-
-
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
-    </div >
+    </div>
   );
-
-}
+};
 
 export default SliderProductComponent;
