5b6004863c0990ec25ce287786607af268d2675a
Milos1988 <vojvoda1988>
2023-04-09 14:10:40 +0200

    Finised search page

--- frontend/src/components/productListComponent.jsx/ProductListComponent.jsx
+++ frontend/src/components/productListComponent.jsx/ProductListComponent.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
-import { getProductData, searchProductData } from "../../service/productService";
+import {
+  getProductData,
+  searchProductData,
+} from "../../service/productService";
 import ProductComponent from "./component/ProductComponent";
 import { Link, useSearchParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -12,21 +15,20 @@ const ProductListComponent = (props) => {
   const [pagination, setPagination] = useState({
     start: 0,
     perPage: 12,
-    allPag: 1
+    allPag: 1,
   });
   const [arrayPagination, setArrayPagination] = useState([]);
   const selectArray = [4, 8, 12, 16, 24, 48];
-  const [active, setActive] = useState(1)
+  const [active, setActive] = useState(1);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(toggleLoader(true))
-    if(!queryParams.get("search")){
-      loadAllProducts()
-    }else{
-      onSearch()
+    dispatch(toggleLoader(true));
+    if (!queryParams.get("search")) {
+      loadAllProducts();
+    } else {
+      onSearch();
     }
-    
   }, [pagination.perPage, pagination.start, pagination.allPag, queryParams]);
 
   const loadAllProducts = () => {
@@ -37,24 +39,37 @@ const ProductListComponent = (props) => {
         const paginationAllNumber = Math.ceil(
           numberPagination / pagination.perPage
         );
-        // console.log(paginationAllNumber )
         let copyPagination = { ...pagination, allPag: paginationAllNumber };
-        setPagination(copyPagination)
+        setPagination(copyPagination);
         setArrayPagination(
           Array.from({ length: paginationAllNumber }, (v, k) => k + 1)
         );
-        dispatch(toggleLoader(false))
       })
-      .catch((err) => console.log(err));
-  }
+      .catch((err) => console.log(err))
+      .finally(() => {
+        dispatch(toggleLoader(false));
+      });
+  };
 
   const onSearch = () => {
-    searchProductData({search: queryParams.get("search")})
-    .then(res =>{ 
-      setProduct(res.data)
-      dispatch(toggleLoader(false))
-    })
-  }
+    searchProductData({ search: queryParams.get("search") })
+      .then((res) => {
+        let numberPagination = res.data.countQuery;
+        setProduct(res.data.data);
+        const paginationAllNumber = Math.ceil(
+          numberPagination / pagination.perPage
+        );
+        let copyPagination = { ...pagination, allPag: paginationAllNumber };
+        setPagination(copyPagination);
+        setArrayPagination(
+          Array.from({ length: paginationAllNumber }, (v, k) => k + 1)
+        );
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        dispatch(toggleLoader(false));
+      });
+  };
 
   return (
     <>
@@ -62,21 +77,19 @@ const ProductListComponent = (props) => {
         {product?.map((el, index) => {
           return (
             <div className={"col-xl-3 col-lg-4 col-sm-6"} key={index}>
-
               <ProductComponent
                 imgUrl={el.imgUrl}
                 title={el.title}
                 price={el.price}
                 id={el._id}
                 dashboard={dashboard}
-              // description={el.description}
               />
             </div>
-
           );
         })}
       </div>
       <div className="mt-5 d-flex justify-content-center paginationField">
+        {!queryParams.get("search") && (
           <div className="dropdown">
             <button
               className="btn btn-secondary dropdown-toggle"
@@ -95,12 +108,12 @@ const ProductListComponent = (props) => {
                       className="dropdown-item"
                       onClick={() => {
                         setPagination({
-                          ...pagination, perPage: el,
-                          start: 0
+                          ...pagination,
+                          perPage: el,
+                          start: 0,
                         });
-                        setActive(1)
+                        setActive(1);
                       }}
-
                     >
                       {el}
                     </Link>
@@ -109,45 +122,62 @@ const ProductListComponent = (props) => {
               })}
             </ul>
           </div>
-        <nav  aria-label="Page navigation example " className="paginationNav">
+        )}
+        <nav aria-label="Page navigation example " className="paginationNav">
           <ul className="pagination justify-content-start">
             <li className="page-item">
-              <Link className="page-link" to="#" aria-label="Previous" onClick={() => {
-                if (pagination.start > 0) {
-                  setPagination({ ...pagination, start: pagination.start - 1 });
-                  setActive(active - 1);
-                  window.scrollTo(0, 0);
-                }
-              }}>
+              <Link
+                className="page-link"
+                to="#"
+                aria-label="Previous"
+                onClick={() => {
+                  if (pagination.start > 0) {
+                    setPagination({
+                      ...pagination,
+                      start: pagination.start - 1,
+                    });
+                    setActive(active - 1);
+                    window.scrollTo(0, 0);
+                  }
+                }}
+              >
                 <span aria-hidden="true">&laquo;</span>
               </Link>
             </li>
             {arrayPagination?.map((el, index) => {
-
               return (
-                <li key={index} className={`page-item ${el === active ? "active" : ""}`} onClick={(e) => {
-                  {
-                    setPagination({ ...pagination, start: el - 1 });
-                    setActive(Number(e.target.text));
-                    window.scrollTo(0, 0);
-                  }
-                }}>
-                  <Link className="page-link" >
-                    {el}
-                  </Link>
+                <li
+                  key={index}
+                  className={`page-item ${el === active ? "active" : ""}`}
+                  onClick={(e) => {
+                    {
+                      setPagination({ ...pagination, start: el - 1 });
+                      setActive(Number(e.target.text));
+                      window.scrollTo(0, 0);
+                    }
+                  }}
+                >
+                  <Link className="page-link">{el}</Link>
                 </li>
               );
             })}
 
             <li className="page-item">
-              <Link className="page-link" href="#" aria-label="Next" onClick={() => {
-                if (pagination.start + 1 < pagination.allPag) {
-                  setPagination({ ...pagination, start: pagination.start + 1 });
-                  setActive(active + 1);
-                  window.scrollTo(0, 0);
-                }
-
-              }}>
+              <Link
+                className="page-link"
+                href="#"
+                aria-label="Next"
+                onClick={() => {
+                  if (pagination.start + 1 < pagination.allPag) {
+                    setPagination({
+                      ...pagination,
+                      start: pagination.start + 1,
+                    });
+                    setActive(active + 1);
+                    window.scrollTo(0, 0);
+                  }
+                }}
+              >
                 <span aria-hidden="true">&raquo;</span>
               </Link>
             </li>
