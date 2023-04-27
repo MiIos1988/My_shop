4ad21952a6c6e492ca25035a7d7ac2d5538f2daa
Milos1988 <vojvoda1988@gmail.com>
2023-04-27 13:26:54 +0200

    Change guard

--- frontend/src/guard/AuthGuardComponent.jsx
+++ frontend/src/guard/AuthGuardComponent.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const AuthGuardComponent = ({ children }) => {
-    const userStore = useSelector((store) => store.userSlicer);
+    const userStore = useSelector((store) => store.userSlicer.user);
     return !userStore ? children : <Navigate to={"/"} />
 }
 
--- frontend/src/guard/PayGuardComponent.jsx
+++ frontend/src/guard/PayGuardComponent.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const PayGuardComponent = ({ children }) => {
-    const userStore = useSelector((store) => store.userSlicer);
+    const userStore = useSelector((store) => store.userSlicer.user);
     return userStore ? children : <Navigate to={"/login?id=1"} />
 }
 
