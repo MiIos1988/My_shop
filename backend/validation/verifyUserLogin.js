fbd9353a7cd2d2b6460ca3509862b390a8cead1d
Milos1988 <vojvoda1988@gmail.com>
2023-04-27 13:50:12 +0200

    Finish verify token for user login

--- backend/validation/verifyUserLogin.js
+++ backend/validation/verifyUserLogin.js
@@ -3,19 +3,16 @@ const { JWT_SECRET_KEY } = require("../config/configToken");
 
 const verifyUserLogin = (req, res, next) => {
   const token = req.headers.authorization;
-  console.log(token);
-  console.log(JWT_SECRET_KEY);
 
   if (!token) {
     return res.status(401).send("You must be logged in");
   }
   try {
-    const decodedToken = jwt.decode(token, JWT_SECRET_KEY)
+    jwt.verify(JSON.parse(token), JWT_SECRET_KEY)
 
-    console.log("----------", decodedToken)
     next();
-  } catch {
-    return res.status(403).send("Your token has expired or is invalid.");
+  } catch (err) {
+    res.status(403).send("Your token has expired or is invalid.");
   }
 };
 
