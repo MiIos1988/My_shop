1a0b5397f631ca3febaff182ddb882ba04264b44
Milos1988 <vojvoda1988@gmail.com>
2023-11-21 21:54:48 +0100

    Error login

--- frontend/src/components/navbar/NavbarComponent.jsx
+++ frontend/src/components/navbar/NavbarComponent.jsx
@@ -17,7 +17,7 @@ const NavbarComponent = () => {
   const navigate = useNavigate();
   const [inputSearch, setInputSearch] = useState("");
   const { t } = useTranslation()
-console.log(userStore?.email )
+console.log(userStore )
   let subtotal = 0;
 
   cartSlicer.cart.map((el, index) => {
