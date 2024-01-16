0a3c0392fc513886d98f54ec9a4502babd66e112
Milos1988 <vojvoda1988@gmail.com>
2024-01-16 23:35:59 +0100

    Converting a component AdminPageComponent to tupescript

--- frontend/src/pages/admin/AdminPageComponent.tsx
+++ frontend/src/pages/admin/AdminPageComponent.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React from "react"
 import { Outlet } from "react-router-dom";
 import LoaderComponent from "../../components/loaderComponent/LoaderComponent";
 import SidebarDashboardComponent from "./SidebarDashboardComponent";
